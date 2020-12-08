dc4a2af12003953defc3cd7fdee2f4eebfa039a8
Bhavani1615 <75241256+Bhavani1615@users.noreply.github.com>
2020-12-08 18:02:37 +0530

    Update TestRunner.java

--- src/test/java/Runners/TestRunner.java
+++ src/test/java/Runners/TestRunner.java
@@ -11,9 +11,9 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 features = "E:\\javaselenium\\DemoWebShopMaven\\src\\test\\java\\features"  // path of feature file
 ,glue={"StepDefinitions"}  //path of step definition class
 ,tags={"@SuccessfulLogin"}  //to run exactly the required scenario
-,plugin = {"pretty", "html:target/cucumber-html-report"}
+//,plugin = {"pretty", "html:target/cucumber-html-report"}
 ,monochrome=true// console output is much more readable
-
+,plugin = {"json:target/cucumber-reports/CucumberTestReport.json"}
 
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
